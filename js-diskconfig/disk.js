a21ffb3672d7a919f5ebf9e8f02df810051d8f03
xinni-ge <xinni.ge@ntt.com>
2016-01-15 10:56:08 +0900

    add steps initial

--- js-diskconfig/disk.js
+++ js-diskconfig/disk.js
@@ -3,6 +3,7 @@ totalwidth=600;
 minwidth=100;
 maxpartition=totalwidth/minwidth;
 disksize=120;
+bccolor=909090;
 
 function calculate(width){
     return width*disksize/totalwidth;
@@ -36,7 +37,9 @@ function loadresize(){$(function() {
   });
 });};//]]> 
 
+
 function delete_partition(){
+  bccolor = bccolor+101010;
   if (count == 2){
       $("#delete-part-btn").prop("disabled",true);
   }
@@ -64,11 +67,13 @@ function delete_partition(){
       $(part_id).html(calculate(width));
       $(part_id).css("margin","1%");
   }
- 
-  
+$("#part-info-list td").each(function(){
+    $(this).css("width", 80/count+"%");
+});
 }
 
 function add_partition(){
+  bccolor = bccolor-101010;
   if (count == maxpartition-1){
       $("#add-part-btn").prop("disabled",true);
   }
@@ -83,23 +88,29 @@ function add_partition(){
 
   $("#outer-div").prepend("<div id=\"div-"+count+"\" class=\"resizable resizable1 ui-resizable\" style=\"top 0px; left:0px;height:50px;\"><div id=\"split-"+count+"\" class=\"ui-resizable-handle ui-resizable-e\" style=\"z-index:1000;\"></div></div>");
   $(".resizable").css("width",width);
-  if (count%2 == 0){
-     $("#div-"+count).css("background-color","lightblue");
-  }
+  //if (count%2 == 0){
+  //   $("#div-"+count).css("background-color","lightblue");
+ //}
+  $("#div-"+count).css("background-color","#"+bccolor);
   $(window).load(loadresize());
   $("#div-1").empty();
   $("#count-block").html(count);
   $("<td id=\"info-div-"+count+"\"></td>").insertAfter($("#info-index"));
   $("<td id=\"length-div-"+count+"\"></td>").insertAfter($("#length-index"));
-  $("<td id=\"tag-div-"+count+"\"><p id=\"tag-"+count+"\" onclick=\"addtag(this)\">click to edit</p></td>").insertAfter($("#tag-index"));
+  $("<td id=\"tag-div-"+count+"\"><p id=\"tag-"+count+"\" class=\"inline\" onclick=\"addtag(this)\"></p></td>").insertAfter($("#tag-index"));
   $("td p").addEffect();
   for (var i=1; i<=count; i=i+1){
       var part_id = "#length-div-"+i;
       var info_id = "#info-div-"+i;
+      var tag_id = "#tag-"+i;
       $(info_id).html(count-i+1);
       $(part_id).html(calculate(width));
+      $(tag_id).html("Disk-"+(count-i+1));
       $(part_id).css("margin","1%");
   }
+  $("#part-info-list td").each(function(){
+      $(this).css("width", 80/count+"%");
+  });
 }
 
 function generate(){
@@ -120,7 +131,7 @@ function generate(){
     }  
     diskconfig["raid_arrays"] = raid_arrays
     diskconfig["filesystems"] = filesystems
-    $("#json-field").html(JSON.stringify(diskconfig));
+    $("#json-field").html(JSON.stringify(diskconfig,null,4));
    }
 
 $.fn.addEffect = function() {
